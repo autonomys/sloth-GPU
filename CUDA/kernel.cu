6d16e1d30359d1a90c99f693f94e8cab9b212583
Özgün Özerk <ozgunozerk@sabanciuniv.edu>
2021-06-26 16:15:52 +0300

    == test added

--- CUDA/kernel.cu
+++ CUDA/kernel.cu
@@ -9,36 +9,79 @@ using namespace std;
 
 __global__ void device_mul(unsigned long long* a, unsigned long long* b, uint128_t* dres)
 {
-	*dres = mul64x2(*a, *b);  // kernel to multiply two 64-bits
+	*dres = mul64x2(*a, *b);
+}
+
+__global__ void device_equals(uint128_t* h1, uint128_t* h2, uint128_t * h3)
+{
+	if (*h1 == *h2) {
+		printf("Fail for a==b\n");
+	}
+	else {
+		printf("Success for a==b\n");
+	}
+	if (*h2 == *h3) {
+		printf("Success for b==c\n");
+	}
+	else {
+		printf("Fail for b==c\n");
+	}
+
 }
 
 int main()
 {
+
+	// Multiplication test start
+
 	unsigned long long a, b;
 
 	a = 1289312831239555555;
 	b = 1290390120391092390;
+
 	uint128_t res;
 
-	unsigned long long * da, * db;
-	uint128_t * dres;
-	// malloc for cuda
-	cudaMalloc(&da, sizeof(unsigned long long));  
+	unsigned long long *da, *db;
+	uint128_t *dres;
+	cudaMalloc(&da, sizeof(unsigned long long));
 	cudaMalloc(&db, sizeof(unsigned long long));
 	cudaMalloc(&dres, 2 * sizeof(unsigned long long));
 
-	// memcpy for cuda
 	cudaMemcpy(da, &a, sizeof(unsigned long long), cudaMemcpyHostToDevice);
 	cudaMemcpy(db, &b, sizeof(unsigned long long), cudaMemcpyHostToDevice);
 
-	// kernel call
 	device_mul << <1, 1 >> > (da, db, dres);
 
-	// memcpy back to CPU
 	cudaMemcpy(&res, dres, 2 * sizeof(unsigned long long), cudaMemcpyDeviceToHost);
 
-	// print
-	cout << bitset<64>(res.high) << endl << bitset<64>(res.low) << endl;
+	cout << "Multiplication test result:\n" << bitset<64>(res.high) << endl << bitset<64>(res.low) << endl;
+
+	// Multiplication test end
+
+	// Equals sign test
+
+	uint128_t h1, h2, h3;
+	uint128_t *d1, *d2, *d3;
+
+	h1.high = std::bitset<64>(std::string("1111111111111111111111111111111111111111111111111111111111111111")).to_ullong();
+	h1.low = std::bitset<64>(std::string("1111111111111111111111110011111111111111001111100111111111111000")).to_ullong();
+
+	h2.high = std::bitset<64>(std::string("1111111111111111111111111111111111111111111111111111111111111111")).to_ullong();
+	h2.low = std::bitset<64>(std::string("1111111111111111111111110011111111111111001111100111111111111100")).to_ullong();
+
+	h3.high = std::bitset<64>(std::string("1111111111111111111111111111111111111111111111111111111111111111")).to_ullong();
+	h3.low = std::bitset<64>(std::string("1111111111111111111111110011111111111111001111100111111111111100")).to_ullong();
+
+	cudaMalloc(&d1, 2 * sizeof(unsigned long long));
+	cudaMalloc(&d2, 2 * sizeof(unsigned long long));
+	cudaMalloc(&d3, 2 * sizeof(unsigned long long));
+
+	cudaMemcpy(d1, &h1, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
+	cudaMemcpy(d2, &h2, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
+	cudaMemcpy(d3, &h3, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
+
+	device_equals << <1, 1 >> > (d1, d2, d3);
+
 
 	return 0;
 }
