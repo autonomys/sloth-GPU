5b4f33ff427572419f661e2f2a64eb0853f8741d
Özgün Özerk <ozgunozerk@sabanciuniv.edu>
2021-06-27 13:27:33 +0300

    boolean operator tests are finished

--- CUDA/kernel.cu
+++ CUDA/kernel.cu
@@ -76,6 +76,49 @@ __global__ void device_lesser(uint128_t* h1, uint128_t* h2, uint128_t * h3)
 }
 
 
+__global__ void device_lesser_64(uint128_t* h4, unsigned long long* a, unsigned long long* b)
+{
+	/* SETUP
+	h4 < b
+	a < h4
+	*/
+	if (*a < *h4) {
+		printf("Success for a < h4\n");
+	}
+	else {
+		printf("Fail for a < h4\n");
+	}
+	if (*b < *h4) {
+		printf("Fail for b < h4\n");
+	}
+	else {
+		printf("Success for b < h4\n");
+	}
+
+}
+
+__global__ void device_equals_64(uint128_t* h4, unsigned long long* a, unsigned long long* b)
+{
+	/* SETUP
+	h4 < b
+	a < h4
+	*/
+	if (*a == *h4) {
+		printf("Fail for a == h4\n");
+	}
+	else {
+		printf("Success for a == h4\n");
+	}
+	if (*b == *h4) {
+		printf("Success for b == h4\n");
+	}
+	else {
+		printf("Fail for b == h4\n");
+	}
+
+}
+
+
 int main()
 {
 
@@ -109,8 +152,10 @@ int main()
 
 	// `==` test start
 
-	uint128_t h1, h2, h3;
-	uint128_t *d1, *d2, *d3;
+	uint128_t h1, h2, h3, h4;
+	uint128_t *d1, *d2, *d3, *d4;
+
+
 
 	h1.high = std::bitset<64>(std::string("1111111111111111111111111111111111111111111111111111111111111111")).to_ullong();
 	h1.low = std::bitset<64>(std::string("1111111111111111111111110011111111111111001111100111111111111000")).to_ullong();
@@ -121,17 +166,29 @@ int main()
 	h3.high = std::bitset<64>(std::string("1111111111111111111111111111111111111111111111111111111111111111")).to_ullong();
 	h3.low = std::bitset<64>(std::string("1111111111111111111111110011111111111111001111100111111111111100")).to_ullong();
 
+	h4.low = std::bitset<64>(std::string("1111111111111111111111110011111111111111001111100111111111111100")).to_ullong();  // 18446743249063149564
+	
+	a = 1289312831239555555;
+	b = 18446743249063149564;
+
 	cudaMalloc(&d1, 2 * sizeof(unsigned long long));
 	cudaMalloc(&d2, 2 * sizeof(unsigned long long));
 	cudaMalloc(&d3, 2 * sizeof(unsigned long long));
+	cudaMalloc(&d4, 2 * sizeof(unsigned long long));
+	cudaMalloc(&da, sizeof(unsigned long long));
+	cudaMalloc(&db, sizeof(unsigned long long));
+
 
 	cudaMemcpy(d1, &h1, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
 	cudaMemcpy(d2, &h2, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
 	cudaMemcpy(d3, &h3, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
+	cudaMemcpy(d4, &h4, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
+	cudaMemcpy(da, &a, sizeof(unsigned long long), cudaMemcpyHostToDevice);
+	cudaMemcpy(db, &b, sizeof(unsigned long long), cudaMemcpyHostToDevice);
 
 	device_equals << <1, 1 >> > (d1, d2, d3);
 
-	// `==` test end
+	// `== (128-128)` test end
 
 	/////////////////////////
 
@@ -143,11 +200,28 @@ int main()
 
 	/////////////////////////
 
-	// `<` test start
+	// `< (128-128)` test start
 
 	device_lesser << <1, 1 >> > (d1, d2, d3);
 
-	// `<` test end
+	// `< (128-128)` test end
+
+	/////////////////////////
+
+	// `< (128-64)` test start
+
+	device_lesser_64 << <1, 1 >> > (d4, da, db);
+
+	// `< (128-64)` test end
+
+	/////////////////////////
+
+	// `< (128-64)` test start
+
+	device_equals_64 << <1, 1 >> > (d4, da, db);
+
+	// `< (128-64)` test end
+
 
 	return 0;
 }
