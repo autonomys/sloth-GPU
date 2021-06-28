d7af64f4475fccdbbc22dd4031e39bac2b02feec
Özgün Özerk <ozgunozerk@sabanciuniv.edu>
2021-06-28 11:55:16 +0300

    test format updated, `-` operators' tests added

--- CUDA/kernel.cu
+++ CUDA/kernel.cu
@@ -123,24 +123,43 @@ __global__ void device_lshift(uint128_t* h4, uint128_t *dres)
 	*dres = *h4 << 1;
 }
 
+__global__ void device_sub_128_128(uint128_t* h1, uint128_t* h2, uint128_t *dres)
+{
+	*dres = *h1 - *h2;
+}
+
+__global__ void device_sub_128_64(uint128_t* h1, uint128_t *dres)
+{
+	*dres = *h1 - 1;
+}
+
 
 int main()
 {
+	// INITIALIZATION
 
-	// Multiplication test start
+	uint128_t h1, h2, h3, h4, h5, h6, h7, res;
+	uint128_t *d1, *d2, *d3, *d4, *d5, *d6, *d7, *dres;
 
 	unsigned long long a, b;
-
-	a = 1289312831239555555;
-	b = 1290390120391092390;
-
-	uint128_t res;
-
 	unsigned long long *da, *db;
-	uint128_t *dres;
+
+
+	cudaMalloc(&d1, 2 * sizeof(unsigned long long));
+	cudaMalloc(&d2, 2 * sizeof(unsigned long long));
+	cudaMalloc(&d3, 2 * sizeof(unsigned long long));
+	cudaMalloc(&d4, 2 * sizeof(unsigned long long));
+	cudaMalloc(&d5, 2 * sizeof(unsigned long long));
+	cudaMalloc(&d6, 2 * sizeof(unsigned long long));
+	cudaMalloc(&d7, 2 * sizeof(unsigned long long));
 	cudaMalloc(&da, sizeof(unsigned long long));
 	cudaMalloc(&db, sizeof(unsigned long long));
 	cudaMalloc(&dres, 2 * sizeof(unsigned long long));
+
+	cout << "Multiplication test start" << endl;
+
+	a = 1289312831239555555;
+	b = 1290390120391092390;
 
 	cudaMemcpy(da, &a, sizeof(unsigned long long), cudaMemcpyHostToDevice);
 	cudaMemcpy(db, &b, sizeof(unsigned long long), cudaMemcpyHostToDevice);
@@ -151,16 +170,9 @@ int main()
 
 	cout << "Multiplication test result:\n" << bitset<64>(res.high) << endl << bitset<64>(res.low) << endl;
 
-	// Multiplication test end
+	cout << "Multiplication test end" << endl;
 
-	/////////////////////////
-
-	// `==` test start
-
-	uint128_t h1, h2, h3, h4;
-	uint128_t *d1, *d2, *d3, *d4;
-
-
+	// PREPERATION FOR OTHER OPERATIONS start
 
 	h1.high = std::bitset<64>(std::string("1111111111111111111111111111111111111111111111111111111111111111")).to_ullong();
 	h1.low = std::bitset<64>(std::string("1111111111111111111111110011111111111111001111100111111111111000")).to_ullong();
@@ -172,86 +184,127 @@ int main()
 	h3.low = std::bitset<64>(std::string("1111111111111111111111110011111111111111001111100111111111111100")).to_ullong();
 
 	h4.low = std::bitset<64>(std::string("1111111111111111111111110011111111111111001111100111111111111100")).to_ullong();  // 18446743249063149564
-	
+
+	h5.high = std::bitset<64>(std::string("0111111111111111111111111111111111111111111111111111111111111111")).to_ullong();
+	h5.low = std::bitset<64>(std::string("1111111111111111111111111111111111111111111111111111111111111111")).to_ullong();
+
+	h6.high = std::bitset<64>(std::string("1000000000000000000000000000000000000000000000000000000000000000")).to_ullong();
+
+	h7 = 1;
+
 	a = 1289312831239555555;
 	b = 18446743249063149564;
-
-	cudaMalloc(&d1, 2 * sizeof(unsigned long long));
-	cudaMalloc(&d2, 2 * sizeof(unsigned long long));
-	cudaMalloc(&d3, 2 * sizeof(unsigned long long));
-	cudaMalloc(&d4, 2 * sizeof(unsigned long long));
-	cudaMalloc(&da, sizeof(unsigned long long));
-	cudaMalloc(&db, sizeof(unsigned long long));
-
 
 	cudaMemcpy(d1, &h1, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
 	cudaMemcpy(d2, &h2, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
 	cudaMemcpy(d3, &h3, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
 	cudaMemcpy(d4, &h4, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
+	cudaMemcpy(d5, &h5, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
+	cudaMemcpy(d6, &h6, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
+	cudaMemcpy(d7, &h7, 2 * sizeof(unsigned long long), cudaMemcpyHostToDevice);
 	cudaMemcpy(da, &a, sizeof(unsigned long long), cudaMemcpyHostToDevice);
 	cudaMemcpy(db, &b, sizeof(unsigned long long), cudaMemcpyHostToDevice);
 
+	// PREPERATION FOR OTHER OPERATIONS end
+
+	cout << "/////////////////////////" << endl;
+
+	cout << "`==` test start" << endl;
+
 	device_equals << <1, 1 >> > (d1, d2, d3);
+	cudaDeviceSynchronize();
 
-	// `== (128-128)` test end
+	cout << "`== (128-128)` test end" << endl;
 
-	/////////////////////////
+	cout << "/////////////////////////" << endl;
 
-	// `>` test start
+	cout << "`>` test start" << endl;
 
 	device_greater << <1, 1 >> > (d1, d2, d3);
+	cudaDeviceSynchronize();
 
-	// `>` test end
+	cout << "`>` test end" << endl;
 
-	/////////////////////////
+	cout << "/////////////////////////" << endl;
 
-	// `< (128-128)` test start
+	cout << "`< (128-128)` test start" << endl;
 
 	device_lesser << <1, 1 >> > (d1, d2, d3);
+	cudaDeviceSynchronize();
 
-	// `< (128-128)` test end
+	cout << "`< (128-128)` test end" << endl;
 
-	/////////////////////////
+	cout << "/////////////////////////" << endl;
 
-	// `< (128-64)` test start
+	cout << "`< (128-64)` test start" << endl;
 
 	device_lesser_64 << <1, 1 >> > (d4, da, db);
+	cudaDeviceSynchronize();
 
-	// `< (128-64)` test end
+	cout << "`< (128 - 64)` test end" << endl;
 
-	/////////////////////////
+	cout << "/////////////////////////" << endl;
 
-	// `== (128-64)` test start
+	cout << "`== (128-64)` test start" << endl;
 
 	device_equals_64 << <1, 1 >> > (d4, da, db);
+	cudaDeviceSynchronize();
 
-	// `== (128-64)` test end
+	cout << "`== (128-64)` test end" << endl;
 
-	/////////////////////////
+	cout << "/////////////////////////" << endl;
 
-	// `>>` test start
+	cout << "`>>` test start" << endl;
 
 	device_rshift << <1, 1 >> > (d3, dres);
-
 	cudaMemcpy(&res, dres, 2 * sizeof(unsigned long long), cudaMemcpyDeviceToHost);
-	cout << "Right shift - input print:\n" << bitset<64>(h3.high) << endl << bitset<64>(h3.low) << endl;
-	cout << "Right shift - result print:\n" << bitset<64>(res.high) << endl << bitset<64>(res.low) << endl;
+	cudaDeviceSynchronize();
 
-	// `>>` test end
+	cout << "Right shift - input print:\n" << bitset<64>(h3.high) << bitset<64>(h3.low) << endl;
+	cout << "Right shift - result print:\n" << bitset<64>(res.high) << bitset<64>(res.low) << endl;
 
-	/////////////////////////
+	cout << "`>>` test end" << endl;
 
-	// `>>` test start
+	cout << "/////////////////////////" << endl;
+
+	cout << "`<<` test start" << endl;
 
 	device_lshift << <1, 1 >> > (d4, dres);
-
 	cudaMemcpy(&res, dres, 2 * sizeof(unsigned long long), cudaMemcpyDeviceToHost);
-	cout << "Left shift - input print:\n" << bitset<64>(h4.high) << endl << bitset<64>(h4.low) << endl;
-	cout << "Left shift - result print:\n" << bitset<64>(res.high) << endl << bitset<64>(res.low) << endl;
+	cudaDeviceSynchronize();
 
-	// `>>` test end
+	cout << "Left shift - input print:\n" << bitset<64>(h4.high) << bitset<64>(h4.low) << endl;
+	cout << "Left shift - result print:\n" << bitset<64>(res.high) << bitset<64>(res.low) << endl;
 
+	cout << "`<<` test end" << endl;
+
+	cout << "/////////////////////////" << endl;
+
+	cout << "`- (128-128)` test start" << endl;
+
+	device_sub_128_128 << <1, 1 >> > (d6, d7, dres);
+	cudaMemcpy(&res, dres, 2 * sizeof(unsigned long long), cudaMemcpyDeviceToHost);
+	cudaDeviceSynchronize();
+
+	cout << "Left shift - input1 print:\n" << bitset<64>(h6.high)<< bitset<64>(h6.low) << endl;
+	cout << "Left shift - input2 print:\n" << bitset<64>(h7.high) << bitset<64>(h7.low) << endl;
+	cout << "Left shift - result print:\n" << bitset<64>(res.high) << bitset<64>(res.low) << endl;
+
+	cout << "`- (128-128)` test end" << endl;
 	
+	cout << "/////////////////////////" << endl;
+
+	cout << "`- (128-64)` test start" << endl;
+
+	device_sub_128_64 << <1, 1 >> > (d6, dres);
+	cudaMemcpy(&res, dres, 2 * sizeof(unsigned long long), cudaMemcpyDeviceToHost);
+	cudaDeviceSynchronize();
+
+	cout << "Left shift - input1 print:\n" << bitset<64>(h6.high) << bitset<64>(h6.low) << endl;
+	cout << "Left shift - input2 print:\n" << "1" << endl;
+	cout << "Left shift - result print:\n" << bitset<64>(res.high) << bitset<64>(res.low) << endl;
+
+	cout << "`- (128-64)` test end" << endl;
 
 	
 
