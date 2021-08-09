09cf38e36683d90a718b93d662202388908df790
Ozgun Ozerk <ozgunozerk@Ozguns-iMac.local>
2021-08-09 20:00:28 +0300

    ptx code and test added, also trials made for optimizations
    typo fix
    
    trial for optimizations, prefetch
    
    prefetch abandoned, try coalesced
    
    coalesced abandoned, trying andy's scenario
    
    trying combined array for iv and piece@

---
+++ CUDA/ptx_test.cu
@@ -0,0 +1,66 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <iostream>
+#include <bitset>
+#include <string>
+
+#include "test_parallel.h"
+
+using namespace std;
+
+int main()
+{
+	int blockSize;      // The launch configurator returned block size
+	int minGridSize;    // The minimum grid size needed to achieve the
+						// maximum occupancy for a full device launch
+	cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, encode_ptx_test);
+
+	minGridSize = minGridSize;
+
+	//minGridSize = 1; blockSize = 1;
+
+	cout << "kernel<<<" << minGridSize << ", " << blockSize << ">>>" << endl;
+
+	u32* piece = (u32*)malloc(sizeof(u32) * 8 * 128 * minGridSize * blockSize);
+
+	u32* d_piece_ptx, * d_expanded_iv_ptx;
+
+    cudaMalloc(&d_piece_ptx, sizeof(u32) * 8 * 128 * minGridSize * blockSize);
+    cudaMalloc(&d_expanded_iv_ptx, sizeof(u32) * 8 * minGridSize * blockSize);
+
+    cudaMemset(d_piece_ptx, 5u, sizeof(u32) * 8 * 128 * minGridSize * blockSize);
+    cudaMemset(d_expanded_iv_ptx, 3u, sizeof(u32) * 8 * minGridSize * blockSize);
+
+    encode_ptx_test<<<minGridSize, blockSize >>>(d_piece_ptx, d_expanded_iv_ptx);
+
+    cudaMemcpy(piece, d_piece_ptx, sizeof(u32) * 8 * 128 * minGridSize * blockSize, cudaMemcpyDeviceToHost);
+
+	cudaDeviceSynchronize();
+
+	/*unsigned char* piece_byte_ptr = (unsigned char*)piece;
+	for (int i = 0; i < 128 * 32; i++)
+	{
+		unsigned number = (unsigned)piece_byte_ptr[i];
+
+		if (number == 0)
+		{
+			cout << "00";
+		}
+		else if (number < 16)
+		{
+			cout << "0";
+			cout << hex << number;
+		}
+		else
+		{
+			cout << hex << number;
+		}
+
+		if (i % 32 == 31)
+			cout << endl;
+	}*/
+
+    return 0;
+}
